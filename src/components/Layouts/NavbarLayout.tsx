7557295c8bdcce706e3811cc8ce16b3b35bd8734
Ahmad Rafi'i <70046808+rafia9005@users.noreply.github.com>
2023-09-17 20:32:45 +0700

    Update NavbarLayout.tsx

--- src/components/Layouts/NavbarLayout.tsx
+++ src/components/Layouts/NavbarLayout.tsx
@@ -102,7 +102,7 @@ export default function NavbarLayout(props: any) {
                 </div>
                 <div className="py-6">
                   <a
-                    href="#"
+                    href="/login"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Log in
