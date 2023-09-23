e530085c1e4fa14ed2e1efdf885750356bcc02ce
Ahmad Rafi'i <70046808+rafia9005@users.noreply.github.com>
2023-09-23 22:28:20 +0700

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBNjA6OuP_K7xOLulxzx5-hw7zLstvfYLM",
-  authDomain: "rafii-ba8e2.firebaseapp.com",
-  projectId: "rafii-ba8e2",
-  storageBucket: "rafii-ba8e2.appspot.com",
-  messagingSenderId: "524025757540",
-  appId: "1:524025757540:web:367504e0d0b2a250a50471",
-  measurementId: "G-D7ZCJG6KPJ"
+  apiKey: "AIzaSyCfJamypbmJgZcEmgjoGPoQruBZ0h0GQpw",
+  authDomain: "pt-ryana-asta-jaya.firebaseapp.com",
+  projectId: "pt-ryana-asta-jaya",
+  storageBucket: "pt-ryana-asta-jaya.appspot.com",
+  messagingSenderId: "489186067565",
+  appId: "1:489186067565:web:07e11663a5073c18c3eb64",
+  measurementId: "G-QK8C31HLPS"
 };
 
 const app = initializeApp(firebaseConfig);
